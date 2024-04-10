9920df016472b5f241024e4ddbb35d29effd6655
KauaFSB <kauaflp234@gmail.com>
2024-04-09 21:10:19 -0300

    Finalização da camada de dados e instalação de pacotes

---
+++ GalloFlix/GalloFlix/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+namespace GalloFlix.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+     public DbSet<Genre> Genres { get; set; }
+}
