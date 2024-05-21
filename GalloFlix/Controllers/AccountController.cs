d8b386e1ca78182f019ac9b2d0e8f63fd10e2174
fuzinelli500280 <riancaiofuzinelli12@gmail.com>
2024-05-21 20:39:24 -0300

    Gerenciando Sistemas de Conta como Login, Logout e inserção de conta

--- GalloFlix/Controllers/AccountController.cs
+++ GalloFlix/Controllers/AccountController.cs
@@ -1,56 +1,96 @@
 using System.Net.Mail;
-using GalloFlix.ViewModels;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+using GalloFlix.ViewModels;
 
 namespace GalloFlix.Controllers;
 
-    public class AccountController : Controller
-    {
-        private readonly ILogger<AccountController> _logger;
-    private readonly object _signInManager;
+public class AccountController : Controller
+{
+    private readonly ILogger<AccountController> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
     private readonly UserManager<IdentityUser> _userManager;
 
     public AccountController(
-            ILogger<AccountController> logger,
-            SignInManager<IdentityUser> signInManager,
-            UserManager<IdentityUser> userManager
+        ILogger<AccountController> logger,
+        SignInManager<IdentityUser> signInManager,
+        UserManager<IdentityUser> userManager
         )
-        {
-            _logger = logger;
-            _signInManager = signInManager;
-            _userManager = userManager;
-        }
-        [HttpGet]
-        public IActionResult Login(string returnUrl)
-        {
-            LoginVM loginVM = new() {
-                ReturnUrl = returnUrl ?? Url.Content("~/")
-            };
-            return View(loginVM);
-        }
+    {
+        _logger = logger;
+        _signInManager = signInManager;
+        _userManager = userManager;
+    }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Login(LoginVM login)
+    [HttpGet]
+    public IActionResult Login(string returnUrl)
+    {
+        LoginVM loginVM = new()
         {
-            if (ModelState.IsValid)
+            ReturnUrl = returnUrl ?? Url.Content("~/")
+        };
+        return View(loginVM);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginVM login)
+    {
+        if (ModelState.IsValid)
+        {
+            string userName = login.Email;
+            if (IsValidEmail(userName))
             {
-                
+                var user = await _userManager.FindByEmailAsync(userName);
+                if (user != null)
+                {
+                    userName = user.UserName;
+                }
             }
-            return View(login);
+
+            var result = await _signInManager.PasswordSignInAsync(
+                userName, login.Password, login.RememberMe, lockoutOnFailure: true
+            );
+            if (result.Succeeded)
+            {
+                _logger.LogInformation($"Usúario {userName} fez login");
+                return LocalRedirect(login.ReturnUrl);
+            }
+
+            if (result.IsLockedOut)
+            {
+                _logger.LogWarning($"Usúario {userName} foi bloqueado");
+                ModelState.AddModelError(string.Empty, "Conta bloqueada! Aguarde alguns minutos para tentar novamente");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Usuáruios e/ou senha inválidos");
+            }
+
         }
-        private static bool IsValidEmail(string email)
+        return View(login);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Logout(){
+        _logger.LogInformation($"Usúario {ClaimTypes.Email} saiu do sistema");
+        await _signInManager.SignOutAsync();
+        return RedirectToAction("Index", "Home");
+    }
+
+    private static bool IsValidEmail(string email)
+    {
+        try
         {
-            try
-            {
-                MailAddress mail = new(email);
-                return true;
-            }
-            catch
-            {
-                
-                return false;
-            }
+            MailAddress mail = new(email);
+            return true;
+        }
+        catch
+        {
+            return false;
         }
     }
+}
